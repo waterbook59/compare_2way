b0ba7bc668ca1cefab50739257d52785092cabcf
waterbook59 <takeshi.m64@gmail.com>
2021-02-08 16:14:01 +0900

    primaryColor,accentColor設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:compare_2way/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,21 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Compare 2way',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+//        primarySwatch: customSwatch,
+        primaryColor: const Color(0xFF363A44),
+        accentColor: const Color(0xFFFF7043),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
