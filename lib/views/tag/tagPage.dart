dafdf4f8e060aa0353dee59488fe02020538bfc8
waterbook59 <takeshi.m64@gmail.com>
2021-02-11 03:46:03 +0900

    homeScreenをStatefulに変更 pageを３つに分割・設定

---
+++ lib/views/tag/tagPage.dart
@@ -0,0 +1,10 @@
+import 'package:compare_2way/style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class TagPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
