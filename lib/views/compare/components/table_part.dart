5e1dac6187a2d2f9f7dd0cf0a53b883c2342e4f4
waterbook59 <takeshi.m64@gmail.com>
2021-02-20 04:58:14 +0900

    Table幅、高さ調整

--- lib/views/compare/components/table_part.dart
+++ lib/views/compare/components/table_part.dart
@@ -3,49 +3,62 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TablePart extends StatelessWidget {
+  TablePart({this.way1Title, this.way2Title});
 
-  TablePart({this.way1Title,this.way2Title});
-  final String  way1Title;
+  final String way1Title;
   final String way2Title;
 
   @override
   Widget build(BuildContext context) {
     final accentColor = CupertinoTheme.of(context).primaryContrastingColor;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:8),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Table(
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
         border: TableBorder.all(),
         children: [
-          TableRow(
-            children: [
-              Container(),
-            IconTitle(
+          TableRow(children: [
+            Container(
+              //figmaでは24
+              height: 30,
+            ),
+            Center(
+              child: IconTitle(
                 title: 'メリット',
-                iconData: Icons.thumb_up ,
+                iconData: Icons.thumb_up,
                 iconColor: accentColor,
               ),
-              IconTitle(
-                title:'デメリット' ,
-                iconData: Icons.thumb_down,
-                iconColor: accentColor,
-              ),
-            ]
-          ),
-          TableRow(
-            children: [
-              Text(way1Title),
-              Text('◎'),
-              Text('✖️'),
-            ]
-          ),
-          TableRow(
-            children: [
-              Text(way2Title),
-            Text('○'),
-            Text('△️'),
-            ]
-
-          ),
+            ),
+            IconTitle(
+              title: 'デメリット',
+              iconData: Icons.thumb_down,
+              iconColor: accentColor,
+            ),
+          ]),
+          TableRow(children: [
+            SizedBox(
+              height: 48,
+              child: Center(
+                  child: Text(
+                way1Title,
+                style: const TextStyle(fontSize: 16),
+              )),
+            ),
+             Center(child: Text('◎')),
+             Center(child: Text('✖️')),
+          ]),
+          TableRow(children: [
+            SizedBox(
+              height: 48,
+              child: Center(
+                  child: Text(
+                way2Title,
+                style: const TextStyle(fontSize: 16),
+              )),
+            ),
+            Center(child: Text('○')),
+            Center(child: Text('△️')),
+          ]),
         ],
       ),
     );
