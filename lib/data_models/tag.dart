fed00a70e94a1ab2ee44d5b4c0e9396f3cf16274
waterbook59 <takeshi.m64@gmail.com>
2021-04-27 05:40:09 +0900

    Tagクラス プロパティをList<ComparisonId>からcomparisonIdに変更

--- lib/data_models/tag.dart
+++ lib/data_models/tag.dart
@@ -1,9 +1,9 @@
 import 'package:compare_2way/data_models/comparison_item_id.dart';
 
 class Tag{
-  Tag({this.comparisonItemIds,this.tagId,this.tagTitle});
+  Tag({this.comparisonItemId,this.tagId,this.tagTitle});
   final int tagId;
   final String tagTitle;
-  final List<ComparisonItemId> comparisonItemIds;
+  final ComparisonItemId comparisonItemId;
 
 }
