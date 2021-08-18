a54cf999f864999cc9bd3ae812970707801f2749
waterbook59 <takeshi.m64@gmail.com>
2021-08-19 05:19:05 +0900

    ReorderableWidgetの余計なところ削除2

--- lib/views/list/componets/reorderble_edit_lsit.dart
+++ lib/views/list/componets/reorderble_edit_lsit.dart
@@ -17,87 +17,7 @@ class ReorderableEditList extends StatefulWidget {
   _ReorderableEditListState createState() => _ReorderableEditListState();
 }
 
-
-class ItemData {
-  ItemData(this.title, this.key,this.id);
-  final String title;
-  // Each item in reorderable list needs stable and unique key
-  final Key key;
-  final String id;
-}
-
-//enum DraggingMode {
-//  iOS,
-//  Android,
-//}
-
-
 class _ReorderableEditListState extends State<ReorderableEditList> {
-
-  //List<CompareViewModel>の内容をList<ItemData>へ変換
-  final List<ItemData> _items =<ItemData>[];
-
-//  _ReorderableEditListState(){
-//    _items = [];
-//    widget.allItemList.map((overview){
-//      return ItemData(overview.itemTitle,ValueKey());
-//    }).toList();
-
-
-    //todo allItemListに変更するとThe getter 'allItemList' was called on null.
-//    for (int i = 0; i < widget.allItemList.length; ++i) {
-//      final overView = widget.allItemList[i];
-//      String label = "List item $i";
-//      if (i == 5) {
-//        label += ". This item has a long label and will be wrapped.";
-//      }
-//      _items.add(ItemData(overView.itemTitle, ValueKey(i)));
-//    }
-//  }
-
-  int _indexOfKey(Key key) {
-//    return _items.indexWhere((ItemData d) => d.key == key);
-    return widget.draggedItems.indexWhere((DraggingItemData d) => d.key == key);
-  }
-
-  bool _reorderCallback(Key item, Key newPosition) {
-    int draggingIndex = _indexOfKey(item);
-    int newPositionIndex = _indexOfKey(newPosition);
-
-
-//    final draggedItem = _items[draggingIndex];
-//    setState(() {
-//      debugPrint("Reordering $item -> $newPosition");
-//      _items.removeAt(draggingIndex);
-//      _items.insert(newPositionIndex, draggedItem);
-
-      final draggedItem =  widget.draggedItems[draggingIndex];
-      setState(() {
-        debugPrint("Reordering $item -> $newPosition");
-        widget.draggedItems.removeAt(draggingIndex);
-        widget.draggedItems.insert(newPositionIndex, draggedItem);
-
-    });
-    return true;
-  }
-
-  void _reorderDone(Key item) {
-//    final draggedItem = _items[_indexOfKey(item)];
-    final draggedItem = widget.draggedItems[_indexOfKey(item)];
-    debugPrint("Reordering finished for ${draggedItem.title}}");
-  }
-
-//  DraggingMode _draggingMode = DraggingMode.iOS;
-
-//  @override
-//  void initState() {
-//    for (int i = 0; i < widget.allItemList.length; ++i) {
-//      final overView = widget.allItemList[i];
-//      _items.add(ItemData(overView.itemTitle, ValueKey(i),
-//          overView.comparisonItemId));
-//    }
-//    super.initState();
-//  }
 
   @override
   Widget build(BuildContext context) {
@@ -107,85 +27,50 @@ class _ReorderableEditListState extends State<ReorderableEditList> {
       child: ListView.builder(
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
-          itemCount:
-//          _items.length,
-          widget.draggedItems.length,
+          itemCount: widget.draggedItems.length,
           itemBuilder: (BuildContext context, int index) {
             return Item(
-              data:
-//            _items[index],
-    widget.draggedItems[index],
+              data: widget.draggedItems[index],
               // first and last attributes affect border drawn during dragging
               isFirst: index == 0,
-//              isLast: index == _items.length - 1,
-              isLast: index == _items.length - 1,
+              isLast: index == widget.draggedItems.length - 1,
               onTap: (){
                 print('onTap!!!');
+                //todo checkIconした時にnotifyListenersでリストが再呼び出しされて順序がもどる
                 final viewModel = Provider.of<CompareViewModel>(context, listen: false)
                   ..checkDeleteIcon( widget.draggedItems[index].id);
               },
-//              draggingMode: _draggingMode,
+
             );
 
           }
       ),
-//      CustomScrollView(
-//        shrinkWrap: true,
-//        physics: const NeverScrollableScrollPhysics(),
-//        // cacheExtent: 3000,
-//        slivers: <Widget>[
-//          SliverAppBar(
-//            actions: <Widget>[
-//              PopupMenuButton<DraggingMode>(
-//                child: Container(
-//                  alignment: Alignment.center,
-//                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-//                  child: Text("Options"),
-//                ),
-//                initialValue: _draggingMode,
-//                onSelected: (DraggingMode mode) {
-//                  setState(() {
-//                    _draggingMode = mode;
-//                  });
-//                },
-//                itemBuilder: (BuildContext context) =>
-//                <PopupMenuItem<DraggingMode>>[
-//                  const PopupMenuItem<DraggingMode>(
-//                      value: DraggingMode.iOS,
-//                      child: Text('iOS-like dragging')),
-//                  const PopupMenuItem<DraggingMode>(
-//                      value: DraggingMode.Android,
-//                      child: Text('Android-like dragging')),
-//                ],
-//              ),
-//            ],
-//            pinned: true,
-//            expandedHeight: 150.0,
-//            flexibleSpace: const FlexibleSpaceBar(
-//              title: const Text('Demo'),
-//            ),
-//          ),
-//          SliverPadding(
-//              padding: EdgeInsets.only(
-//                  bottom: MediaQuery.of(context).padding.bottom),
-//              sliver: SliverList(
-//                delegate: SliverChildBuilderDelegate(
-//                      (BuildContext context, int index) {
-//                    return Item(
-//                      data: _items[index],
-//                      // first and last attributes affect border drawn during dragging
-//                      isFirst: index == 0,
-//                      isLast: index == _items.length - 1,
-//                      draggingMode: _draggingMode,
-//                    );
-//                  },
-//                  childCount: _items.length,
-//                ),
-//              )),
-//        ],
-//      ),
     );
   }
+
+  int _indexOfKey(Key key) {
+    return widget.draggedItems.indexWhere((DraggingItemData d) => d.key == key);
+  }
+
+  bool _reorderCallback(Key item, Key newPosition) {
+    final draggingIndex = _indexOfKey(item);
+    final newPositionIndex = _indexOfKey(newPosition);
+
+    final draggedItem =  widget.draggedItems[draggingIndex];
+    //todo リストの位置を変更した形でDBへ保存が必要
+    setState(() {
+      debugPrint("Reordering $item -> $newPosition");
+      widget.draggedItems.removeAt(draggingIndex);
+      widget.draggedItems.insert(newPositionIndex, draggedItem);
+    });
+    return true;
+  }
+
+  void _reorderDone(Key item) {
+    final draggedItem = widget.draggedItems[_indexOfKey(item)];
+    debugPrint("Reordering finished for ${draggedItem.title}}");
+  }
+
 }
 
 
@@ -195,15 +80,12 @@ class Item extends StatelessWidget {
     this.isFirst,
     this.isLast,
     this.onTap,
-//    this.draggingMode,
   });
 
-//  final ItemData data;
   final DraggingItemData data;
   final bool isFirst;
   final bool isLast;
   final VoidCallback onTap;
-//  final DraggingMode draggingMode;
 
   Widget _buildChild(BuildContext context, ReorderableItemState state) {
     final primaryColor = Theme.of(context).primaryColor;
@@ -230,10 +112,7 @@ class Item extends StatelessWidget {
     }
 
     ///ListTile右側の並び替え部
-//    Widget
     final dragHandle =
-//    draggingMode == DraggingMode.iOS
-//        ?
     ReorderableListener(
       child: Container(
         padding: EdgeInsets.only(right: 18.0, left: 18.0),
@@ -243,9 +122,7 @@ class Item extends StatelessWidget {
         ),
       ),
     );
-//        : Container();
 
-//    Widget
     final content = Container(
       decoration: decoration,
       child: SafeArea(
@@ -265,7 +142,6 @@ class Item extends StatelessWidget {
                       child: EditListTile(
                         title: data.title,
                         onTap: onTap,
-//                            checkDeleteIcon(context,data.id),
                         icon:
                         viewModel.deleteItemIdList.contains(data.id)
                        ? Icon(
@@ -281,17 +157,6 @@ class Item extends StatelessWidget {
                       ),
                     ),
                   ),
-//                  Expanded(
-//                      child: Padding(
-//                        padding:
-//                        EdgeInsets.symmetric(vertical: 14.0, horizontal: 14.0),
-//                        child: Text(data.title,
-//                            style: Theme
-//                                .of(context)
-//                                .textTheme
-//                                .subtitle1),
-//                      )),
-                  // Triggers the reordering
                   ///ListTile右側の並び替え部
                   dragHandle,
                 ],
@@ -299,13 +164,6 @@ class Item extends StatelessWidget {
             ),
           )),
     );
-
-
-//    if (draggingMode == DraggingMode.Android) {
-//      content = DelayedReorderableListener(
-//        child: content,
-//      );
-//    }
 
     return content;
   }
@@ -316,10 +174,4 @@ class Item extends StatelessWidget {
         key: data.key, //
         childBuilder: _buildChild);
   }
-
-//  void checkDeleteIcon(BuildContext context, String itemId) {
-//    print('タップで削除アイコン切替');
-//    final viewModel = Provider.of<CompareViewModel>(context, listen: false)
-//      ..checkDeleteIcon(itemId);
-//  }
 }
