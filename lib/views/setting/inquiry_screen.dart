68bbb408ffe709be50a6216f96304251b3df333b
waterbook59 <takeshi.m64@gmail.com>
2022-01-31 06:01:33 +0900

    問い合わせページ入力 余計な行削除

--- lib/views/setting/inquiry_screen.dart
+++ lib/views/setting/inquiry_screen.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-//todo 濁点入らない、英字入力で日本語へ戻ってしまう、予測変換されない
 class InquiryScreen extends StatefulWidget {
 
   @override
@@ -49,6 +48,8 @@ class _InquiryScreenState extends State {
   @override
   void initState() {
     super.initState();
+    // 濁点入らない、英字入力で日本語へ戻ってしまう、予測変換されない場合
+    ///Androidの時のみ追加
     if (Platform.isAndroid) {
       WebView.platform = SurfaceAndroidWebView();
     }
@@ -57,85 +58,6 @@ class _InquiryScreenState extends State {
   @override
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
-
-//    return  CupertinoPageScaffold(
-//        navigationBar: CupertinoNavigationBar(
-//          backgroundColor: primaryColor,
-//          middle: const Text(
-//            '設定',
-//            style: middleTextStyle,
-//          ),
-//          trailing:  GestureDetector(
-//            child: const Icon(
-//              //CupertinoIcons.keyboard_chevron_compact_downない
-//              Icons.keyboard_hide,
-//              color: Colors.white,
-//            ),
-//            onTap:()=> _unFocusTap(context),
-//          ),
-//        ),
-//        child: Scaffold(//todo Consumer必要?
-//          body: connectionStatus == true
-//
-//              ?
-//          SingleChildScrollView(
-//            child: FutureBuilder<dynamic>(
-//              future: check(), // Future or nullを取得
-//                builder: (BuildContext context, AsyncSnapshot snapshot) {
-//                  return
-//                  IndexedStack(
-//                    index: position,
-//                    children: [
-//                      WebView(
-//                        initialUrl: 'https://waterbook.sakura.ne.jp/inquiry.html',
-//                        javascriptMode: JavascriptMode.unrestricted, // JavaScriptを有効化
-//                        onWebViewCreated:
-//                            (WebViewController webViewController) {
-//                          _controller.complete(webViewController);
-//                        },
-//                        key: key,
-//                        onPageFinished: doneLoading,
-//                        // indexを０にしてWebViewを表示
-//                        onPageStarted: startLoading, // indexを1にしてプログレスインジケーターを表示
-//                      ),
-//                      // プログレスインジケーターを表示
-//                      Container(
-//                        child: Center(
-//                          child: CircularProgressIndicator(
-//                              backgroundColor: Colors.blue),
-//                        ),
-//                      ),
-//                    ],
-//                  );
-//                }),
-//          )
-//
-//          /// インターネットに接続されていない場合の処理
-//              : SafeArea(
-//            child: Center(
-//              child: Column(
-//                children: [
-//                  Padding(
-//                    padding: EdgeInsets.only(
-//                      top: 120,
-//                      bottom: 20,
-//                    ),
-//                    child: Container(),
-//                  ),
-//                  Padding(
-//                    padding: EdgeInsets.only(
-//                      bottom: 20,
-//                    ),
-//                    child: Text(
-//                      'インターネットに接続されていません',
-//                    ),
-//                  ),
-//                ],
-//              ),
-//            ),
-//          ),
-//        ));
-
 
     return  FutureBuilder<dynamic>(
         future: check(), // Future or nullを取得
@@ -147,48 +69,30 @@ class _InquiryScreenState extends State {
                 '設定',
                 style: middleTextStyle,
               ),
-              trailing:  GestureDetector(
-                child: const Icon(
-                  //CupertinoIcons.keyboard_chevron_compact_downない
-                  Icons.keyboard_hide,
-                  color: Colors.white,
-                ),
-                onTap:()=> _unFocusTap(context),
-              ),
             ),
             child: Scaffold(
-//            appBar: AppBar(
-//              title: Text('不具合を報告'),
-//              leading:         GestureDetector(
-//            child: const Icon(
-//            //CupertinoIcons.keyboard_chevron_compact_downない
-//            Icons.keyboard_hide,
-//              color: Colors.white,
-//            ),
-//          ),
-
-
-            body: connectionStatus == true
-            //todo LayoutBuilder&SingleChildScrollView追加もエラー：RenderAndroidView object was given an infinite size during layout.
-               ?
-               IndexedStack(
+              body: connectionStatus == true
+               ? IndexedStack(
               index: position,
               children: [
                       WebView(
                         initialUrl: 'https://waterbook.sakura.ne.jp/inquiry.html',
-                        javascriptMode: JavascriptMode.unrestricted, // JavaScriptを有効化
+                        javascriptMode: JavascriptMode.unrestricted,
                         onWebViewCreated:
                             (WebViewController webViewController) {
                           _controller.complete(webViewController);
                         },
                         key: key,
-                        onPageFinished: doneLoading, /// indexを０にしてWebViewを表示
-                        onPageStarted: startLoading, /// indexを1にしてプログレスインジケーターを表示
+                        /// indexを０にしてWebViewを表示
+                        onPageFinished: doneLoading,
+                        /// indexを1にしてプログレスインジケーターを表示
+                        onPageStarted: startLoading,
                       ),
                       // プログレスインジケーターを表示
                       Container(
-                        child: Center(
-                          child: CircularProgressIndicator(backgroundColor: Colors.blue),
+                        child: const Center(
+                          child: CircularProgressIndicator(
+                              backgroundColor: Colors.blue),
                         ),
                       ),
               ],
@@ -200,13 +104,13 @@ class _InquiryScreenState extends State {
                 child: Column(
                   children: [
                     Padding(
-                      padding: EdgeInsets.only(
+                      padding:const EdgeInsets.only(
                         top: 120,
                         bottom: 20,
                       ),
                       child: Container(),
                     ),
-                    Padding(
+                   const Padding(
                       padding: EdgeInsets.only(
                         bottom: 20,
                       ),
@@ -225,13 +129,6 @@ class _InquiryScreenState extends State {
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(DiagnosticsProperty('connectionStatus', connectionStatus));
-  }
-
-
-  void _unFocusTap(BuildContext context) {
-    //setStateしてない
-    print('unFocus!');
-    FocusScope.of(context).unfocus();
   }
 
 }
