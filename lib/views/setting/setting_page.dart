1511807a8f4409c152f14849964e704cfdcfc81c
waterbook59 <takeshi.m64@gmail.com>
2023-02-24 06:30:24 +0900

    SettingPageののnull safety対応 -UserPolicyScreenでのweb_view_flutter ver4.0対応

--- lib/views/setting/setting_page.dart
+++ lib/views/setting/setting_page.dart
@@ -3,23 +3,22 @@ import 'package:compare_2way/style.dart';
 import 'package:compare_2way/views/setting/license_page.dart';
 import 'package:compare_2way/views/setting/privacy_policy_screen.dart';
 import 'package:compare_2way/views/setting/user_policy_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:settings_ui/settings_ui.dart';
 
-
 class SettingPage extends StatefulWidget {
+  const SettingPage({Key? key}) : super(key: key);
+
   @override
   _SettingPageState createState() => _SettingPageState();
 }
 
 class _SettingPageState extends State<SettingPage> {
-
   @override
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
-    final accentColor= Theme.of(context).accentColor;
 
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
@@ -38,26 +37,31 @@ class _SettingPageState extends State<SettingPage> {
       ),
       child: Scaffold(
 //        backgroundColor: CupertinoTheme.of(context).scaffoldBackgroundColor,
-        body:
-        Column(
+        body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 16,),
+            const SizedBox(
+              height: 16,
+            ),
+
             ///Androidはこちらがいい
 //            const Padding(
 //              padding: EdgeInsets.symmetric(horizontal: 8),
 //              child: Text('このアプリについて', textAlign: TextAlign.left,
 //                style: subTitleTextStyle,),
 //            ),
+            ///iOSはこちらがいい
             SettingsList(
               shrinkWrap: true,
               sections: [
                 SettingsSection(
-                  title: 'このアプリについて',///iOSはこちらがいい
-                  titleTextStyle: subTitleTextStyle,
+                  title: const Text(
+                    'このアプリについて',
+                    style: subTitleTextStyle,
+                  ),
                   tiles: [
                     SettingsTile(
-                      title: '要望・不具合に関する報告',
+                      title: const Text('要望・不具合に関する報告'),
 //                      subtitle: 'English',
                       leading: const Icon(CupertinoIcons.envelope),
                       onPressed: (context) {
@@ -65,47 +69,57 @@ class _SettingPageState extends State<SettingPage> {
                       },
                     ),
                     SettingsTile(
-                      title: 'このアプリを評価する',
+                      title: const Text('このアプリを評価する'),
 //                      subtitle: 'English',
                       leading: const Icon(CupertinoIcons.star_circle_fill),
 //                      trailing:const Icon(Icons.arrow_forward_ios),
-                      onPressed: (context)=>_requestReview(),
+                      onPressed: (context) => _requestReview(),
                     ),
                     SettingsTile(
-                      title: '利用規約',
+                      title: const Text('利用規約'),
 //                      subtitle: 'English',
                       leading: const Icon(CupertinoIcons.text_justify),
 //                      trailing:const Icon(Icons.arrow_forward_ios),
                       onPressed: (context) {
-                      Navigator.of(context).push(MaterialPageRoute<void>(
-                          builder: (_) => UserPolicyScreen(),
-                        ));
+                        Navigator.of(context).push(
+                          MaterialPageRoute<void>(
+                            builder: (_) => UserPolicyScreen(),
+                          ),
+                        );
                       },
                     ),
                     SettingsTile(
-                      title: 'プライバシーポリシー',
+                      title: const Text('プライバシーポリシー'),
 //                      subtitle: 'English',
                       leading: const Icon(CupertinoIcons.doc_text),
 //                      trailing:const Icon(Icons.arrow_forward_ios),
                       onPressed: (context) {
-                        Navigator.of(context).push(MaterialPageRoute<void>(
-                          builder: (_) => PrivacyPolicyScreen(),
-                        ));
+                        Navigator.of(context).push(
+                          MaterialPageRoute<void>(
+                            builder: (_) => PrivacyPolicyScreen(),
+                          ),
+                        );
                       },
                     ),
                     SettingsTile(
-                      title: 'ライセンス',
+                      title: const Text('ライセンス'),
 //                      subtitle: 'English',
-                      leading: Image.asset('assets/images/license.png',width: 26,color: Colors.black54,),
+                      leading: Image.asset(
+                        'assets/images/license.png',
+                        width: 26,
+                        color: Colors.black54,
+                      ),
 //                      trailing:const Icon(Icons.arrow_forward_ios),
                       onPressed: (context) {
-                    Navigator.of(context).push(MaterialPageRoute<void>(
-                      builder: (_) => MyLicensePage(),
-                    ));
+                        Navigator.of(context).push(
+                          MaterialPageRoute<void>(
+                            builder: (_) => MyLicensePage(),
+                          ),
+                        );
                       },
                     ),
                     SettingsTile(
-                      title: 'バージョン',
+                      title: const Text('バージョン'),
 //                      subtitle: 'English',
                       leading: const Icon(Icons.language),
 //                      trailing:const Icon(Icons.arrow_forward_ios),
@@ -121,7 +135,6 @@ class _SettingPageState extends State<SettingPage> {
             ),
           ],
         ),
-
       ),
     );
   }
@@ -132,19 +145,17 @@ class _SettingPageState extends State<SettingPage> {
 
 //flutter_email_senderを使ってメールを送信
   Future<void> flutterEmailSenderMail() async {
-    final Email email = Email(
-      body: '${Template().text}',
+    final email = Email(
+      body: Template().text,
       subject: '転ばぬ先の2way：サポート',
       recipients: ['waterbook.tokyo@gmail.com'],
     );
 
     await FlutterEmailSender.send(email);
   }
-
 }
 
 //本文のテンプレート的な
 class Template {
-  String text =
-      'ご意見・ご要望をこちらにご記入ください';
+  String text = 'ご意見・ご要望をこちらにご記入ください';
 }
